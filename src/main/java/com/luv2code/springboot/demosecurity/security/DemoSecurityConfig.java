a7f1512ef05ed95cacc201eee4adf0c5c31f8921
cuongvk <cuongvk.hcm@vnpt.vn>
2023-06-23 00:36:03 +0700

    MVC Security  starter project: cơ bản về user trong DB (plain text password)

--- src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -6,11 +6,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
                 .username("john")
@@ -29,6 +33,18 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }*/
+
+
+    /**
+     *
+     * @param dataSource
+     * @return
+     * @implNote add support for JDBC (plain text)
+     */
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
 
